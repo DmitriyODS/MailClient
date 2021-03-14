8cdf7264c7c35f1241e138a17ceb21d746e9dac7
DmitriyODS <osipovskij.dima@yandex.ru>
2021-03-15 02:26:47 +0300

    Добавление логики работы с БД

---
+++ dbQuarry/Db.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <sqlite3.h>
+#include <unistd.h>
+
+#include "Db.h"
+#include "../tools/tools.h"
+
+
+Db::Db(string root_dir, string name_stg)
+        : m_name_root_dir(std::move(root_dir)), m_name_file_base(std::move(name_stg)) {
+    _init();
+}
+
+void Db::_init() {
+    fs::path workdir = fs::current_path() / m_name_root_dir;
+
+    bool err{};
+
+    if (!fs::exists(workdir)) {
+        err = fs::create_directory(workdir);
+        if (err) {
+            logger("Error! Failed to create working directory for base!", LevelLog::criticalError);
+            exit(1);
+        }
+    }
+
+    m_root_dir = workdir;
+
+    if (chdir(m_root_dir.c_str())) {
+        err = fs::create_directory(workdir);
+        if (err) {
+            logger("Error, unable to change working directory!", LevelLog::criticalError);
+            exit(1);
+        }
+    }
+}
+
+Db &Db::connectDb() {
+    if (sqlite3_open(m_name_file_base.c_str(), &m_db)) {
+        string err_msg{
+                "Error creating or connecting to the database!",
+                sqlite3_errmsg(m_db)
+        };
+        logger(err_msg, LevelLog::criticalError);
+
+        sqlite3_close(m_db);
+        exit(1);
+    }
+    m_connect = true;
+
+    return *this;
+}
+
+Db &Db::disconnectDb() {
+    sqlite3_close(m_db);
+
+    m_connect = false;
+
+    return *this;
+}
+
+string Db::getNameRootDir() const {
+    return m_name_root_dir;
+}
+
+string Db::getNameFileDb() const {
+    return m_name_file_base;
+}
+
+bool Db::isConnect() const {
+    return m_connect;
+}
+
+Db &Db::saveObject(ParsingInterface &object) {
+    //TODO: Сохранение объекта в БД
+
+    return *this;
+}
+
+Db &Db::restoreObject(ParsingInterface &object) {
+    //TODO: Восстановление объекта из БД
+
+    return *this;
+}
+
+Db &Db::finalize() {
+    //TODO: Какие - то операции по закреплению данных в БД
+
+    return *this;
+}
+
+Db::~Db() {
+    disconnectDb();
+    finalize();
+}
---
+++ dbQuarry/Db.h
@@ -0,0 +1,61 @@
+#ifndef LR2_DB_H
+#define LR2_DB_H
+
+#include <fstream>
+#include <string>
+#include <sqlite3.h>
+#include <experimental/filesystem>
+
+#include <ParsingInterface.h>
+
+
+using std::fstream;
+using std::string;
+
+namespace fs = std::experimental::filesystem::v1;
+
+class Db {
+public:
+    Db() = delete;
+
+    Db(const Db &dbTransaction) = delete;
+
+    Db &operator=(const Db &dbTransaction) = delete;
+
+    Db(string root_dir, string name_file_base);
+
+    ~Db();
+
+    Db &connectDb();
+
+    Db &disconnectDb();
+
+    string getNameRootDir() const;
+
+    string getNameFileDb() const;
+
+    bool isConnect() const;
+
+    Db &saveObject(ParsingInterface &object);
+
+    Db &restoreObject(ParsingInterface &object);
+
+    Db &finalize();
+
+private:
+    // заголовок объекта соединения с БД
+    sqlite3 *m_db{};
+
+    // строка ошибки
+    char *m_err_base{};
+
+    string m_name_file_base{};
+    string m_name_root_dir{};
+    fs::path m_root_dir{};
+    bool m_connect{};
+
+    void _init();
+};
+
+
+#endif //LR2_DB_H
